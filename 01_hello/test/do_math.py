2ae303bff7a08fd8bbf4f6f5972b9c1fd38dc3b0
dcflores99 <davidflo204@gmail.com>
2021-09-17 18:11:15 -0400

    updated main function to include command line arguments

--- 01_hello/test/do_math.py
+++ 01_hello/test/do_math.py
@@ -1,14 +1,35 @@
+# main.py
+import sys
+
 def addition(a, b):
     return a + b
 
 
-def addition(a, b):
+def subtraction(a, b):
     return a - b
 
 
-def addition(a, b):
+def multiplication(a, b):
     return a * b
 
 
-def addition(a, b):
+def division(a, b):
     return a / b
+
+
+def main():
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if sys.argv[2] == "+":
+        print(addition(a, b))
+    if sys.argv[2] == "-":
+        print(subtraction(a, b))
+    if sys.argv[2] == "*":
+        print(multiplication(a, b))
+    if sys.argv[2] == "/":
+        print(division(a, b))
+
+
+
+if __name__ == "__main__":
+    main()
