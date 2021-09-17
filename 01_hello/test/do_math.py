371d90a234415aba73b0a4db7bf2817d03de52ec
dcflores99 <davidflo204@gmail.com>
2021-09-17 17:53:01 -0400

    added math functions

---
+++ 01_hello/test/do_math.py
@@ -0,0 +1,14 @@
+def addition(a, b):
+    return a + b
+
+
+def addition(a, b):
+    return a - b
+
+
+def addition(a, b):
+    return a * b
+
+
+def addition(a, b):
+    return a / b
---
+++ 01_hello/test/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""tests for hello.py"""
+
+import os
+from subprocess import getstatusoutput, getoutput
+
+prg = './do_math.py'
